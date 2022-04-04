967eb0af2a168ad3ede630f96e29757f98e1bb6a
SunDust23 <julik23@bk.ru>
2022-04-04 10:26:28 +0500

    Start
    The first setup, test command

---
+++ index.js
@@ -0,0 +1,17 @@
+const TelegramBot = require('node-telegram-bot-api');
+require('dotenv').config();
+const token = process.env.BOT_TOKEN;
+const bot = new TelegramBot(token, { polling: true } );
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Привет, Друг!');
+  });
+
+// bot.onText(/\/echo (.+)/, (msg, match) => {
+
+// 	const chatId = msg.chat.id
+// 	const resp = match[1]
+
+// 	bot.sendMessage(chatId, resp)
+// })
